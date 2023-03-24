541500bd54a3dfdc7a421374c177becf4dc09011
Kai Norman Clasen <k.clasen@protonmail.com>
2023-03-25 00:24:48 +0100

    Start work on flake that provides package

--- flake.nix
+++ flake.nix
@@ -1,22 +1,41 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
+    # flake-parts.url = "github:hercules-ci/flake-parts"
   };
   outputs = { self, nixpkgs }:
-    let pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
     in
-    {
-      devShell.x86_64-linux =
-        pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [
-            rustc
-            cargo
-            rustfmt
-            clippy
-            rust-analyzer
-            linuxKernel.packages.linux_latest_libre.usbip
-          ];
-          USBIP_TCP_PORT = 5000;
+    rec {
+      packages.x86_64-linux = {
+        usbip_wrapper = pkgs.rustPlatform.buildRustPackage {
+          pname = "usbip-wrapper";
+          version = "v0.1.0";
+
+          src = self;
+
+          # cargoSha256 = pkgs.lib.fakeSha256;
+          cargoSha256 = "sha256-35wXNIUg01RX4qNRSYF6PXooRzqRpAkRTGuXEJd6MCs=";
+
+          meta = with pkgs.lib; {
+            description = "A simple usbip wrapper";
+          };
         };
+        defaultPackage = packages.usbip_wrapper;
+
+        devShell.x86_64-linux =
+          pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [
+              rustc
+              cargo
+              rustfmt
+              clippy
+              rust-analyzer
+              linuxKernel.packages.linux_latest_libre.usbip
+            ];
+            USBIP_TCP_PORT = 5000;
+          };
+      };
     };
 }
