f509de67fe6e7987444d622955580dfce3082d55
Kai Norman Clasen <k.clasen@protonmail.com>
2023-03-31 10:19:21 +0200

    Further work on Flake Module to allow easy importing

--- flake.nix
+++ flake.nix
@@ -6,8 +6,38 @@
   outputs = { self, nixpkgs }:
     let
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      system = "x86_64-linux";
     in
     rec {
+      nixosModules.default = import ./usbip_wrapper.nix self;
+      nixosModules.usbip_wrapper = import ./usbip_wrapper.nix self;
+
+      # Define the NixOS test configuration
+      testConfig = {
+        # The VM configuration
+        config = {
+          inherit system;
+          # Include the package in the system configuration
+          imports = [ nixosModules.default ];
+          environment.systemPackages = [ packages."${system}".default ];
+        };
+      };
+
+      # TODO: Understand how to run nixosTest!
+      # Define the NixOS test using the `nixosTest` function
+      # https://nix.dev/tutorials/integration-testing-using-virtual-machines
+      # https://github.com/NixOS/nixpkgs/blob/master/nixos/tests/login.nix
+      testResult = nixpkgs.nixosTest {
+        name = "my-test";
+        system = testConfig.system;
+        config = testConfig.config;
+        # extraDiskImages = [ nixpkgs.nixosImage { system = testConfig.system; } ];
+        # env = with testConfig; {
+        #   # Export the test configuration to the VM
+        #   testConfig = builtins.toJSON testConfig;
+        # };
+      };
+
       packages.x86_64-linux = {
         usbip_wrapper = pkgs.rustPlatform.buildRustPackage {
           pname = "usbip-wrapper";
@@ -23,10 +53,7 @@
           };
         };
         default = packages.x86_64-linux.usbip_wrapper;
-
       };
-      nixosModules.default = import ./usbip_wrapper.nix self;
-      nixosModules.usbip_wrapper = import ./usbip_wrapper.nix self;
 
       # chatgpt says:
       # nixosModules.usbip_wrapper = { ... }: { systemd.packages = [] }
@@ -42,6 +69,39 @@
             linuxKernel.packages.linux_latest_libre.usbip
           ];
           USBIP_TCP_PORT = 5000;
+        };
+      nixosConfigurations =
+        let
+          lib = nixpkgs.lib;
+        in
+        {
+          vm = lib.makeOverridable lib.nixosSystem {
+            inherit system;
+            modules =
+              let
+                pkgs = import nixpkgs { inherit system; };
+              in
+              [
+                # import the usbip_wrapper modules
+                nixosModules.default
+                ({ pkgs, config, ... }: {
+                  system.stateVersion = "22.11";
+                  services.usbip_wrapper = {
+                    enable = true;
+                    port = 5000;
+                  };
+                  # This should be configurable to test different kernel version interacting with each other
+                  # boot.kernelPackages = pkgs.linuxPackages_latest;
+                  boot.kernelPackages = pkgs.linuxPackages;
+                  # Test that the flake doesn't break the user-defined configuration
+                  boot.kernelModules = [ "zfs" ];
+                  boot.extraModulePackages = with config.boot.kernelPackages; [ zfs ];
+                  environment.systemPackages = [
+                    pkgs.usbutils
+                  ];
+                })
+              ];
+          };
         };
     };
 }
--- usbip_wrapper.nix
+++ usbip_wrapper.nix
@@ -3,6 +3,7 @@
 flake: { config, lib, pkgs, ... }:
 let
   cfg = config.services.usbip_wrapper;
+  usbip_wrapper = flake.packages.x86_64-linux.usbip_wrapper;
 in
 {
   imports = [
@@ -10,20 +11,86 @@ in
 
   options = {
     services.usbip_wrapper = {
-      enable = mkEnableOption ''
+      enable = lib.mkEnableOption ''
         A usbip configuration wrapper
       '';
+      # could set port here
+      # and usb device, I guess
+      package = lib.mkOption {
+        type = lib.types.package;
+        default = usbip_wrapper;
+        description = ''
+          The usbip_wrapper package to use with the service.
+        '';
+      };
+
+      port = lib.mkOption {
+        type = lib.types.int;
+        default = 5000;
+        description = ''
+          The port that the usbip server/client will communicate over.
+        '';
+      };
     };
-    # could set port here
-    # and usb device, I guess
   };
 
   config = lib.mkIf cfg.enable {
-    systemd.services.usbip_wrapper = {
-      description = "usbip-wrapper";
-      serviceConfig = {
-        ExecStart = "${cfg.package}/bin/usbip_wrapper";
+    # This seems to work! This is amazing!
+    # I can simply add these as a dependency from within the flake
+    # and continue to minimize the dependencies for the end-user flake file!
+    # Nix will merge the lists during the resolving phase!
+    boot.extraModulePackages = with config.boot.kernelPackages; [ usbip ];
+    boot.kernelModules = [ "usbip_host" "vhci-hcd" ];
+
+    systemd.services.usbip_server = {
+      description = "Start usbip server.";
+      # TODO: Re-enable after a few tests!
+
+      # requires = [ "usbip_server_timeout.timer" ];
+      # after = [ "usbip_server_timeout.timer" ];
+      # requires
+      # Stop the server when the timeout service is started,
+      # which is controlled by a systemd timer unit!
+      # conflicts = [ "usbip_server_timeout.service" ];
+      environment = {
+        USBIP_TCP_PORT = "5555";
+        # systemd recommends against PID files!
+        # USBIP_DAEMON_PID_PATH = "/var/run/usbipd.pid";
       };
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = ''
+          ${cfg.package}/bin/usbip_wrapper start-usb-hoster
+        '';
+        # TODO: Disable pid file if not necessary
+        # TODO: Think where the unhost command should be run! 
+        # just to cleanly shut down
+        # or does shutting down the server take care of it?
+        Restart = "no";
+        # Basic Hardening
+        NoNewPrivileges = "yes";
+        PrivateTmp = "yes";
+        # PrivateDevices = "no";
+        # Limit access to only contain relevant USB devices!
+        # DeviceAllow = [ "char-usb/*" "char-usb_device/*" ];
+        # DevicePolicy = "closed"; # TODO: Understand this better!
+        # DynamicUser = "true"; # need to explicitely create user for simpler secret management
+        # ProtectSystem = "strict";
+        # why at all read-only ? Whose home?
+        # ProtectHome = "read-only";
+        # ProtectControlGroups = "yes";
+        # ProtectKernelModules = "yes";
+        # ProtectKernelTunables = "yes";
+        # RestrictAddressFamilies = "AF_UNIX AF_INET AF_INET6 AF_NETLINK";
+        # RestrictNamespaces = "yes";
+        # RestrictRealtime = "yes";
+        # RestrictSUIDSGID = "yes";
+        # MemoryDenyWriteExecute = "yes";
+        # LockPersonality = "yes";
+      };
+      # Can this be done within the module?
+      # guarantee that the correct usbip version is used and in sync with linux kernel!
+      path = [ "${config.boot.kernelPackages.usbip}" ];
     };
   };
 }
